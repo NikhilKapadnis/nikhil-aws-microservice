e11b0965ac5ecd63bd9e709ba978f8c9f48ae597
Nikhil Kap <110217822+NikhilKapadnis@users.noreply.github.com>
2026-04-20 16:48:49 +0100

    Fix duplicate route_table_id in public_1 association

--- terraform/main.tf
+++ terraform/main.tf
@@ -77,7 +77,6 @@ resource "aws_route_table" "public" {
 resource "aws_route_table_association" "public_1" {              # #route table association public 1
   route_table_id = aws_route_table.public.id
   subnet_id      = aws_subnet.public_1.id
-  route_table_id = aws_route_table.public.id
 }
 resource "aws_route_table_association" "public_2" {
   subnet_id      = aws_subnet.public_2.id                      #route table association public 2
